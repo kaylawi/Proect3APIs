ee891511bf6775c6eea9169515d1214abfd645c0
Kayla <kaylawi@umich.edu>
2017-11-04 14:11:24 -0400

    commit message

---
+++ twitter_info.py
@@ -0,0 +1,5 @@
+consumer_key = "TEN6NaK2lTjStPsi4IqDEMjwB" 
+consumer_secret = "G17lZYmedhPGbHTYyaoQ0oLPN0OukYeu5UQwYUXC1khz9aGBcz"
+access_token = "921138800943927305-CMX2dtpDturd7GymE1Dkg2tlL6nSlRH"
+access_token_secret = "qArGQqZ1pPcOqT0FREjCWLzprRklOcE23qsQaCLlzXbGY"
+
